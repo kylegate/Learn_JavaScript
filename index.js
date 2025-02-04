7b1e075dd970b09d596a94addacb32c62927e19c
kylegate <kylewebbb@gmail.com>
2025-02-04 16:53:38 -0500

    started to learn JS

---
+++ index.js
@@ -0,0 +1,16 @@
+// var prompt = require('readline-sync');
+// var answer = prompt.question('Say something? ');
+console.log(answer)
+
+var people = ['1','2','3']
+console.log(people[0])
+
+var dict = {
+    'key' : 'value',
+    'key2' : 'value2'
+}
+
+console.log(dict['key']) // value
+console.log(dict['value']) // undefined
+
+// new comment?
